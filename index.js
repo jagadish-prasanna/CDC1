1c4f69a762ef029b6d4ef0e09bee99a1423c6b69
Jagadish <jagadish_p@hotmail.com>
2016-02-13 13:23:24 -0800

    Contagious Disease Coordination
    Initial Commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');    
+
+var mongodbUri = 'mongodb://cdc-user:$$cdcuser@ds059205.mongolab.com:59205/cdc';
+//var mongodbUri = 'localhost:27017';
+
+
+var db = mongoose.connection;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.get('/api/search',function (req,res){
+	res.send('Hello');
+});
+app.post('/api/users', function(req, res) {
+   mongoose.connect(mongodbUri);
+  db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+
+  // Create song schema
+  var diseaseSchema = mongoose.Schema({
+    providerId: String,
+    providerName: String,
+    latitude: Number,
+    longitude: Number,
+    patientPhoneNumber:String,
+    patientAddress:String,
+    patientSymptoms:String,
+    patientDiseaseId:String,
+    diagnoisisDate:Date
+    
+  });
+
+  // Store song documents in a collection called "songs"
+  var Disease = mongoose.model('disease', diseaseSchema);
+
+  // Create seed data
+  var diseaseincidence = new Disease({
+    providerId: req.body.providerId,
+    providerName: req.body.providerName,
+    latitude:  req.body.latitude,
+    longitude: req.body.longitude,
+    patientPhoneNumber: req.body.patientPhoneNumber,
+    patientAddress: req.body.patientAddress,
+    patientSymptoms: req.body.patientSymptoms,
+    patientDiseaseId: req.body.patientDiseaseId,
+    diagnoisisDate: req.body.diagnoisisDate
+  });
+
+  diseaseincidence.save();
+
+    res.send(req.body.providerId);
+
+
+ }); 
+
+
+});
+app.listen(2000);
---
+++ readme.md
@@ -0,0 +1,15 @@
+This project is in response of various diseases outbreak in various parts of the world. The project attempts to use the "social" data out there to 
+capture the information from healthcare professionals (nurse/doctors etc.)
+at the source.
+  
+ This attempts to use the following technologies
+
+ a) Smart Phones which are universally available.
+ b) Use openid (to identify the health care professional)
+ c) Geolocation of the incidence and time/date
+
+
+  This is just a part of solution - giving an API for the capture.
+  There must be a phone app. which allows the professionals to record the incidence.
+  Governement agencies will be able to use a "Real Time Heat map" to determine when thier resources needs to be targeted and there by making an impact on the over all spread of the disease.
+  
